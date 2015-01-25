22cec1182d2359f52eea0f393ede9c290e58c14a
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-24 21:58:36 -0500

    More series. Just for the sake of committing. Need to complete Real World Haskell to make anything cool.

--- series.hs
+++ series.hs
@@ -5,3 +5,10 @@ geometricSeries a = a + (1/a) + (1/(1/a)) + (1/(1/(1/a)))
 
 
 --Need a way to terminate inifite series when they've reached a level of approximation of some number that I think is adequately precise
+
+--This makes no sense whatsoever. It's a harmonic series up to +1/6, but then adding some random integer? For shame!
+harmonicSeries b = 1 + (1/2) + (1/4) + (1/5) + (1/6) + b
+
+
+harmonicSeries b = b + (1/b) + (1/b-1) + (1/b-2) + (1/b-3)
+
