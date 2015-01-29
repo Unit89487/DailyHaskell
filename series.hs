9344a8e7505ee909a35cab330aae303e4963a933
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-28 22:35:11 -0500

    Sine

--- series.hs
+++ series.hs
@@ -10,17 +10,23 @@ geometricSeries a = a + (1/a) + (1/(1/a)) + (1/(1/(1/a)))
 harmonicSeries b = 1 + (1/2) + (1/4) + (1/5) + (1/6) + b
 
 
-harmonicSeries b = b + (1/b) + (1/b-1) + (1/b-2) + (1/b-3)
+harmonicSeries d = d + (1/d) + (1/d-1) + (1/d-2) + (1/d-3)
 
 
 
 --continued fraction
-continuedFraction z = z + (1/z) + (1/(1/z)) + (1/(1/(1/z)) + (1/(1/(1/(1/z))))
+continuedFraction z = z + (1/z) + (1/(1/z)) + (1/(1/(1/z))) + (1/(1/(1/(1/z))))
 
 
-summation v = if v < 1000 then v + (summation (v + 1) else v 
+summation v = if v < 1000 then v + (summation (v + 1)) else v 
 
 
 
 --Definitely not a series 
 whatever x = sqrt(4 - x^2)
+
+
+--Sine
+
+sineSquared q = (sin(q))^2
+sinePlusPi h = sin h + pi 
